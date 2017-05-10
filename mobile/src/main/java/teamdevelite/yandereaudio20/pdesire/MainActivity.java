56086a8726443e2ebee0ccc6e487d7572198fd37
PDesire <tristan.marsell@t-online.de>
2017-05-10 18:56:13 +0200

    Initial Release

---
+++ mobile/src/main/java/teamdevelite/yandereaudio20/pdesire/MainActivity.java
@@ -0,0 +1,64 @@
+package teamdevelite.yandereaudio20.pdesire;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById (R.id.toolbar);
+        setSupportActionBar (toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById (R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle (
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener (toggle);
+        toggle.syncState ();
+
+        NavigationView navigationView = (NavigationView) findViewById (R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener (this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById (R.id.drawer_layout);
+        if (drawer.isDrawerOpen (GravityCompat.START)) {
+            drawer.closeDrawer (GravityCompat.START);
+        } else {
+            super.onBackPressed ();
+        }
+    }
+
+    @SuppressWarnings ("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId ();
+
+        if (id == R.id.nav_service) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setComponent(new ComponentName("com.meli.pdesire.projectmeliaudioeffects","com.meli.pdesire.projectmeliaudioeffects.SettingsActivity"));
+            startActivity(intent);
+        } else if (id == R.id.nav_pdesireaudio) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setComponent(new ComponentName("com.meli.pdesire.projectmeliaudioeffects","com.meli.pdesire.projectmeliaudioeffects.PDesireAudioActivity"));
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById (R.id.drawer_layout);
+        drawer.closeDrawer (GravityCompat.START);
+        return true;
+    }
+}
