d6105160be53d3b276a1e3a618b705057c3481a1
PDesire <tristan.marsell@t-online.de>
2017-05-20 13:28:35 +0200

    Project CodeBleed: src: Convert Java to Kotlin
    The main codes of the app got translated to Kotlin for now
    
    Signed-off-by: Tristan Marsell <tristan.marsell@t-online.de>

---
+++ mobile/src/main/java/teamdevelite/yandereaudio20/pdesire/MainActivity.kt
@@ -0,0 +1,63 @@
+package teamdevelite.yandereaudio20.pdesire
+
+/**
+ * Created by PDesire on 20.05.2017.
+ */
+
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        val toggle = ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.setDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById(R.id.nav_view) as NavigationView
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        val id = item.itemId
+
+        if (id == R.id.nav_service) {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.component = ComponentName("com.meli.pdesire.projectmeliaudioeffects", "com.meli.pdesire.projectmeliaudioeffects.SettingsActivity")
+            startActivity(intent)
+        } else if (id == R.id.nav_pdesireaudio) {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.component = ComponentName("com.meli.pdesire.projectmeliaudioeffects", "com.meli.pdesire.projectmeliaudioeffects.PDesireAudioActivity")
+            startActivity(intent)
+        }
+
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
