d598af7fa4ad984af057482f5cf3156e32de40f5
PDesire <tristan.marsell@t-online.de>
2017-05-26 22:29:39 +0200

    CodeBleed: Warning for Lucky Patcher users
    Better you do not use Lucky Patcher anymore...
    
    Signed-off-by: Tristan Marsell <tristan.marsell@t-online.de>

--- mobile/src/main/java/teamdevelite/yandereaudio20/pdesire/MainActivity.kt
+++ mobile/src/main/java/teamdevelite/yandereaudio20/pdesire/MainActivity.kt
@@ -4,6 +4,7 @@ package teamdevelite.yandereaudio20.pdesire
  * Created by PDesire on 20.05.2017.
  */
 
+import android.app.AlertDialog
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
@@ -14,8 +15,44 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private fun checkLuckyPatcher(): Boolean {
+        if (packageExists("com.dimonvideo.luckypatcher")) {
+            return true
+        }
+
+        if (packageExists("com.chelpus.lackypatch")) {
+            return true
+        }
+
+        if (packageExists("com.android.vending.billing.InAppBillingService.LACK")) {
+            return true
+        }
+
+        if (packageExists("com.android.vending.billing.InAppBillingService.LOCK")) {
+            return true
+        }
+
+        return false
+    }
+
+    private fun packageExists(packageName: String): Boolean {
+        try {
+            val info = this.packageManager.getApplicationInfo(packageName, 0) ?: // No need really to test for null, if the package does not
+                    // exist it will really rise an exception. but in case Google
+                    // changes the API in the future lets be safe and test it
+                    return false
+
+            return true
+        } catch (ex: Exception) {
+            // If we get here only means the Package does not exist
+        }
+
+        return false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +68,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         val navigationView = findViewById(R.id.nav_view) as NavigationView
         navigationView.setNavigationItemSelectedListener(this)
+        if (checkLuckyPatcher()) {
+            val pirate = AlertDialog.Builder(this)
+            pirate.setTitle("PIRATE DETECTED")
+            pirate.setMessage("If I would be you, I would uninstall Lucky Patcher and never use it anymore... \n \n I am sure you want to use Project Meli... \n \n Just saying: I know how to make Project Meli unusable, \n so you will not get any sound enhancements")
+            pirate.create()
+            pirate.show()
+        }
     }
 
     override fun onBackPressed() {
